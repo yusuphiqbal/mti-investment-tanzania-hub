6b2275a204f448ab1bf4efc9660635a00df378d0
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2024-07-09 11:25:37 +0300

    Added meta description to head

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="description" content="Where great ideas come to life">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/2.7.2/css/lightgallery-bundle.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="css/app.css" rel="stylesheet">
